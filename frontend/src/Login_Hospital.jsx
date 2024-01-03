b7baa95054ea12439314c5a242ed99eee1340236
Konrad <xvx2402@gmail.com>
2024-01-03 15:37:01 +0100

    Add frontend template

---
+++ frontend/src/Login_Hospital.jsx
@@ -0,0 +1,9 @@
+const Login_Hospital = () => {
+    return (
+      <div className="login_hospital">
+        <h2>Login as Hospital</h2>
+      </div>
+    );
+  }
+   
+  export default Login_Hospital;
---
+++ frontend/src/Login_User.jsx
@@ -0,0 +1,9 @@
+const Login_User = () => {
+    return (
+      <div className="login_user">
+        <h2>Login as User</h2>
+      </div>
+    );
+  }
+   
+  export default Login_User;
