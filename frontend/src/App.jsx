0b8989dde8188cbef22a47debd186b9db6eb0c0a
szymonjozwiakzip <jozwiakszymon03@gmail.com>
2024-01-05 15:12:13 +0100

    ASN-8 few changes in frontend template and reactive input component add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,49 @@
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Home from './Home'
-import LoginUser from './LoginUser'
-import LoginHospital from './LoginHospital'
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './Home';
+import LoginUser from './LoginUser';
+import LoginHospital from './LoginHospital';
 import UiInput from './components/ui/UiInput.jsx';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (value) => {
+    setInputValue(value);
+    console.log(value);
+  };
 
   fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())
-      .then(json => console.log(json))
+    .then(response => response.json())
+    .then(json => console.log(json));
 
   fetch('https://jsonplaceholder.typicode.com/users/2')
-      .then(res => res.json())
-      .then(j => console.log(j))
+    .then(res => res.json())
+    .then(j => console.log(j));
 
   return (
-    
     <BrowserRouter>
-    <Routes>
-    <Route
+      <Routes>
+        <Route
           exact
           path="/"
           element={
             <div>
-              <UiInput label="input:" placeholder="Input" />
+              <UiInput
+                label="input:"
+                placeholder="Input"
+                inputValue={inputValue}
+                onChange={handleInputChange}
+              />
             </div>
           }
         />
-      <Route path="login" element={<LoginUser />}/>
-      <Route path="login-hos" element={<LoginHospital />}/>
-    </Routes>
+        <Route path="login" element={<LoginUser />} />
+        <Route path="login-hos" element={<LoginHospital />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
