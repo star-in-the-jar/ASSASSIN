deb08a3b25c791bb9bef3f29a821b061dbdc50a7
Pmik13 <patryk.mike18@gmail.com>
2024-01-05 22:37:14 +0100

    Dodanie button component

---
+++ frontend/src/components/Button.jsx
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Button.css'; 
+
+const Button = ({ variant, label, onClick }) => {
+  const buttonClass = `button ${variant}`;
+
+  return (
+    <button className={buttonClass} onClick={onClick}>
+      {label}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  variant: PropTypes.oneOf(['blue', 'purple']).isRequired,
+  label: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+};
+
+export default Button;
