6ce21623ac0ffd2ffac54d4b3a976ce7300baf0f
Pmik13 <patryk.mike18@gmail.com>
2024-01-08 21:07:17 +0100

    Dodanie LoginUser.css i zmiana kodu w LoginUser i LoginHospital na widok z uzyciem komponentow.Zmienilem szerokosc Buttona zeby pasowal do szerokosci w widoku

--- frontend/src/LoginHospital.jsx
+++ frontend/src/LoginHospital.jsx
@@ -1,9 +1,47 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Button from './components/Button';
+import UiInput from './components/ui/UiInput';
+import './LoginUser.css'; 
+import { Link } from 'react-router-dom';
+
 const Login_Hospital = () => {
-    return (
-      <div className="login_hospital">
-        <h2>Login as Hospital</h2>
+  const navigate = useNavigate();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  
+  const handleButtonClick = () => {
+    console.log("działa?");
+    navigate('/login');
+  };
+
+  return (
+      <div className="login_view">
+        <Link to="/login">
+          <h4>&lt;powrót</h4>
+        </Link>
+        <h1 className="hospital">Logowanie jako <br></br> szpital</h1>
+        <UiInput
+         label="login szpitala"
+         placeholder=""
+         inputValue={username}
+         onChange={setUsername}
+       />
+       <br></br>
+       <br></br>
+       <br></br>
+       <UiInput
+         label="haslo"
+         placeholder=""
+         inputValue={password}
+         onChange={setPassword}
+       />
+       <br></br>
+       <br></br>
+       <br></br>
+       <Button variant="blue" label="Zaloguj się" onClick={handleButtonClick} />
+        <h5>Zaloguj się jako pacjent</h5>
       </div>
-    );
-  }
-   
+  );
+};
   export default Login_Hospital;
--- frontend/src/LoginUser.jsx
+++ frontend/src/LoginUser.jsx
@@ -1,9 +1,48 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Button from './components/Button';
+import UiInput from './components/ui/UiInput';
+import './LoginUser.css'; 
+import { Link } from 'react-router-dom';
+
 const Login_User = () => {
-    return (
-      <div className="login_user">
-        <h2>Login as User</h2>
+  const navigate = useNavigate();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleButtonClick = () => {
+    console.log("działa?");
+    navigate('/login');
+  };
+
+  return (
+      <div className="login_view">
+        <Link to="#" onClick={() => window.history.goBack()}>
+          <h4>&lt;powrót</h4>
+        </Link>
+        <h1>Logowanie</h1>
+        <UiInput
+         label="login pacjenta"
+         placeholder=""
+         inputValue={username}
+         onChange={setUsername}
+       />
+       <br></br>
+       <br></br>
+       <br></br>
+       <UiInput
+         label="haslo"
+         placeholder=""
+         inputValue={password}
+         onChange={setPassword}
+       />
+       <br></br>
+       <br></br>
+       <br></br>
+       <Button variant="blue" label="Zaloguj się" onClick={handleButtonClick} />
+        <h5>Zaloguj się jako szpital</h5>
       </div>
-    );
-  }
-   
-  export default Login_User;
+  );
+};
+
+export default Login_User;
