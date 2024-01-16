ad123867903fb065c0e493e5dc55d62c140eea98
Władysław Kuczerenko <wladyslawkuczerenko@mbp-wadysaw.home>
2024-01-17 00:00:56 +0100

    Prepare on stage

--- frontend/src/LoginUser.jsx
+++ frontend/src/LoginUser.jsx
@@ -1,47 +1,59 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Button from './components/ui/Button';
-import UiInput from './components/ui/UiInput';
-import './LoginUser.css'; 
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import Button from "./components/ui/Button";
+import UiInput from "./components/ui/UiInput";
+import "./LoginUser.css";
+import { Link } from "react-router-dom";
+import Modal from "./components/ui/Modal";
+import { useNavigate } from "react-router-dom";
 
 const Login_User = () => {
   const navigate = useNavigate();
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleButtonClick = () => {
-    console.log("działa?");
-    navigate('/login');
+    setIsModalOpen(true);
+  };
+
+  const onCodeFilled = (code) => {
+    console.log(code);
   };
 
   return (
-      <div className="login_view">
-        <Link to="#" onClick={() => window.history.goBack()}>
-          <h4>&lt;powrót</h4>
-        </Link>
-        <h1>Logowanie</h1>
-        <UiInput
-         label="login pacjenta"
-         placeholder=""
-         inputValue={username}
-         onChange={setUsername}
-       />
-       <br></br>
-       <br></br>
-       <br></br>
-       <UiInput
-         label="haslo"
-         placeholder=""
-         inputValue={password}
-         onChange={setPassword}
-       />
-       <br></br>
-       <br></br>
-       <br></br>
-       <Button variant="blue" label="Zaloguj się" onClick={handleButtonClick} />
-        <h5>Zaloguj się jako szpital</h5>
-      </div>
+    <div className="login_view">
+      <Link onClick={() => navigate(-1)}>
+        <h4 className="login_go_back">&lt;powrót</h4>
+      </Link>
+      <h1 className="login_view_title">Logowanie</h1>
+      <UiInput
+        label="login pacjenta"
+        placeholder=""
+        inputValue={username}
+        onChange={setUsername}
+      />
+      <br></br>
+      <br></br>
+      <br></br>
+      <UiInput
+        label="haslo"
+        placeholder=""
+        inputValue={password}
+        onChange={setPassword}
+      />
+      <br></br>
+      <br></br>
+      <br></br>
+      <Button variant="blue" label="Zaloguj się" onClick={handleButtonClick} />
+      <Link to="/login-hos" className="login_other_view_link">
+        Zaloguj się jako szpital
+      </Link>
+
+      {isModalOpen && (
+        <Modal closeModal={setIsModalOpen} onCodeFilled={onCodeFilled} />
+      )}
+    </div>
   );
 };
 
