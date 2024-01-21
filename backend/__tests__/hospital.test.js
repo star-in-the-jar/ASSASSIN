a13a9d732803c44b287d23473654dffdf8d94d0d
starswaterbrook <mwkm00@gmail.com>
2024-01-21 14:43:59 +0100

    Fixed test

--- backend/__tests__/hospital.test.js
+++ backend/__tests__/hospital.test.js
@@ -1,20 +1,9 @@
 const request = require('supertest');
-const app = require('../routes/hospitalRoutes');
-const db = require('../db/dbConnection');
+const app = require('../index');
 
 describe("Test the root path", () => {
     test("It should response the GET method", async () => {
-        const response = request(app).get("http://localhost:3000/api/hospitals");
-        expect(response.statusCode).toBe(200);
+        const response = await request(app).get("/api/hospitals");
+        await expect(response.statusCode).toBe(200);
     });
 });
-
-// describe('Test the addLike method', () => {
-//     beforeAll(() => {
-//         mongoDB.connect();
-//     });
-//
-//     afterAll((done) => {
-//         mongoDB.disconnect(done);
-//     });
-// }
